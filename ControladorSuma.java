8e81ac29e082a6d3d6461da0fa0cbb27b2fbd3da
Fer <fmorenomarzal@gmail.com>
2024-08-05 18:57:01 +0200

    añadiendo alert y modelo

--- ControladorSuma.java
+++ ControladorSuma.java
@@ -6,8 +6,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import modelo.Suma;
 
 public class ControladorSuma implements Initializable {
 	
@@ -35,6 +37,27 @@ public class ControladorSuma implements Initializable {
 	
 	@FXML
 	private void sumar(ActionEvent event) {
+		
+		try {
+		int op1=Integer.parseInt(this.txtOp1.getText());
+		int op2=Integer.parseInt(this.txtOp2.getText());
+		
+		Suma suma=new Suma(op1,op2);
+		
+		int resultado=suma.suma();
+		
+		this.txtResultado.setText(resultado+"");
+		
+		}catch(NumberFormatException e) {
+			
+			Alert alert= new Alert(Alert.AlertType.ERROR);
+			alert.setHeaderText(null);
+			alert.setTitle("Error");
+			alert.setContentText("Formato incorrecto");
+			alert.showAndWait();
+			
+		}
+		
 		
 	}
 	
